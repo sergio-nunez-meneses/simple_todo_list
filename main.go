953d56627211b73c44c8475e6c86914eb5e61a44
sergio-nunez-meneses <snunezmeneses@proton.me>
2024-11-12 12:44:55 +0100

    Add base function handleUserInput & Update main function (main.go)

--- main.go
+++ main.go
@@ -2,10 +2,10 @@
 Gestion de liste de tâches (To-Do List)
 
 Objectif : Créer un programme en Go qui permet de gérer une liste de tâches avec les opérations suivantes :
-	1.	Ajouter une tâche.
-	2.	Afficher toutes les tâches.
-	3.	Marquer une tâche comme terminée.
-	4.	Supprimer une tâche.
+	1.	Ajouter une tâche
+	2.	Afficher toutes les tâches
+	3.	Marquer une tâche comme terminée
+	4.	Supprimer une tâche
 
 Étapes :
     1.	Créer une structure Task
@@ -17,8 +17,8 @@ Objectif : Créer un programme en Go qui permet de gérer une liste de tâches a
         - ajouterTache(titre string): Ajoute une nouvelle tâche à la liste avec le titre fourni
         - afficherTaches(): Affiche toutes les tâches, en précisant celles qui sont terminées et celles qui sont en
             attente
-        - marquerCommeTerminee(id int): Marque une tâche comme terminée en utilisant son ID.
-        - supprimerTache(id int): Supprime une tâche de la liste en utilisant son ID.
+        - marquerCommeTerminee(id int): Marque une tâche comme terminée en utilisant son ID
+        - supprimerTache(id int): Supprime une tâche de la liste en utilisant son ID
     3.	Interaction avec l’utilisateur :
     •	Dans main, proposer un menu textuel simple pour permettre à l’utilisateur de choisir une action :
         1. Ajouter une tâche
@@ -48,6 +48,10 @@ var nextId = 1
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	handleUserInput(scanner)
+}
+
+func handleUserInput(scanner *bufio.Scanner) {
 	for {
 		scanner.Scan()
 		fmt.Printf(scanner.Text())
